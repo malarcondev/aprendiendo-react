5c204d3a4d0f3066af778556db2c60ce2166f9ac
Marcos Alarcon <proveedormarcos.alarcon@sura.pe>
2023-06-15 18:31:17 -0500

    add useRef

--- 03-hook-app/src/main.jsx
+++ 03-hook-app/src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { FocusScreen } from './04-useRef/FocusScreen'
 
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <CounterWithCustomHook/> */}
     {/* <SimpleForm/> */}
     {/* <FormWithCustomHook/> */}
-    <MultipleCustomHooks/>
+    {/* <MultipleCustomHooks/> */}
+    <FocusScreen/>
   </React.StrictMode>,
 )
