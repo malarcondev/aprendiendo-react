c328eb9a7ea8f6167db15991a5d1ba9975fa9f70
Marcos Alarcon <proveedormarcos.alarcon@sura.pe>
2023-06-27 23:13:58 -0500

    add useLayoutEffect + useMemo

--- 03-hook-app/src/main.jsx
+++ 03-hook-app/src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { FocusScreen } from './04-useRef/FocusScreen'
+import { Layout } from './05-useLayoutEffect/Layout'
+import { Memorize } from './06-memos/Memorize'
+import { MemoHook } from './06-memos/MemoHook'
 
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
@@ -9,6 +11,7 @@ import { FocusScreen } from './04-useRef/FocusScreen'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { FocusScreen } from './04-useRef/FocusScreen'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -18,6 +21,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <SimpleForm/> */}
     {/* <FormWithCustomHook/> */}
     {/* <MultipleCustomHooks/> */}
-    <FocusScreen/>
+    {/* <FocusScreen/> */}
+    {/* <Layout/> */}
+    {/* <Memorize/> */}
+    <MemoHook/>
   </React.StrictMode>,
 )
